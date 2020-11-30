7caf674df82e2c8e965d4463e2b3ac0fc4ec22b6
Marisa You <marisayou2017@u.northwestern.edu>
2020-11-29 22:13:44 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, string) {
+    const len = string.length
+    return drivers.filter(function(driver) {
+        return driver.substring(0, len) === string
+    })
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(function(driver) {
+        return driver.name === string
+    })
+}
